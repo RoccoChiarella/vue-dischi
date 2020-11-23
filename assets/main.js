82b44856b727be0b8907ebee2f8a7e0c17b3bbd3
Rocco <rocco.chiarella.rc@gmail,com>
2020-11-23 15:24:21 +0100

    Select genre

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,20 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            selected: '',
             songs: []
         },
+        methods: {
+            changeGenre: function() {
+
+            }
+        },
         mounted() {
-                axios
-                    .get('https://flynn.boolean.careers/exercises/api/array/music')
-                    .then((response) => {
-                        this.songs = response.data.response;
-                });
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((response) => {
+                    this.songs = response.data.response;
+            });
         }
     }
 );
